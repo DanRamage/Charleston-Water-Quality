f9917cc374cd83699d1fa41da215e66a62b6b95d
DanRamage <ChiefDan@gmail.com>
2017-05-31 14:03:57 -0400

    Init checkin

---
+++ scripts/get_historical_nexrad_data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash#!/bin/bash
+
+source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
+
+python /home/xeniaprod/scripts/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/config/mb_historic_data_config.ini --ImportData=/mnt/waterquality/xmrg/2013/qpexmrg_201301,/mnt/waterquality/xmrg/2013/qpexmrg_201302,/mnt/waterquality/xmrg/2013/qpexmrg_201303,/mnt/waterquality/xmrg/2013/qpexmrg_201304,/mnt/waterquality/xmrg/2013/qpexmrg_201305,/mnt/waterquality/xmrg/2013/qpexmrg_201306,/mnt/waterquality/xmrg/2013/qpexmrg_201307,/mnt/waterquality/xmrg/2013/qpexmrg_201308,/mnt/waterquality/xmrg/2013/qpexmrg_201309,/mnt/waterquality/xmrg/2013/qpexmrg_201310,/mnt/waterquality/xmrg/2013/qpexmrg_201311,/mnt/waterquality/xmrg/2013/qpexmrg_201312
