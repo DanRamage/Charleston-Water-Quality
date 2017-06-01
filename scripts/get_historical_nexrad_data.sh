3eee7e58f937fd591b7b1b65bae7b40fa45f6ebf
DanRamage <ChiefDan@gmail.com>
2017-06-01 14:10:55 -0400

    Init checkin

--- scripts/get_historical_nexrad_data.sh
+++ scripts/get_historical_nexrad_data.sh
@@ -2,4 +2,4 @@
 
 source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
 
-python /home/xeniaprod/scripts/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Charleston-Water-Quality/config/chs_historical_build_config.ini --ImportData=/mnt/waterquality/xmrg/2015
+python /home/xeniaprod/scripts/commonfiles/python/wqXMRGProcessing.py --ConfigFile=/home/xeniaprod/scripts/Charleston-Water-Quality/config/chs_historical_build_config.ini --ImportData=/mnt/waterquality/xmrg/2016/jan,/mnt/waterquality/xmrg/2016/feb,/mnt/waterquality/xmrg/2016/mar,/mnt/waterquality/xmrg/2016/apr,/mnt/waterquality/xmrg/2016/may,/mnt/waterquality/xmrg/2016/jun,/mnt/waterquality/xmrg/2016/jul,/mnt/waterquality/xmrg/2016/aug,/mnt/waterquality/xmrg/2016/sep,/mnt/waterquality/xmrg/2016/oct,/mnt/waterquality/xmrg/2016/nov,/mnt/waterquality/xmrg/2016/dec
