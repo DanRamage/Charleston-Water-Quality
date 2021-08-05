76e6ea096473322fdaa48ab584be8a82dbef478d
DANRAMAGE <dan@inlet.geol.sc.edu>
2021-08-05 09:48:55 -0400

    Use the old emit() output plugins.

--- scripts/output_plugin/email_output_plugin.py
+++ scripts/output_plugin/email_output_plugin.py
@@ -5,102 +5,69 @@ from mako.template import Template
 from mako import exceptions as makoExceptions
 from smtp_utils import smtpClass
 import os
-if sys.version_info[0] < 3:
-  import ConfigParser
-else:
-  import configparser as ConfigParser
-
 import logging.config
-import time
-from data_collector_plugin import data_collector_plugin
+from output_plugin import output_plugin
 
-class email_output_plugin(data_collector_plugin):
+class email_output_plugin(output_plugin):
+  def __init__(self):
+    output_plugin.__init__(self)
+    self.logger = logging.getLogger(__name__)
+
 
   def initialize_plugin(self, **kwargs):
     try:
-
-      #data_collector_plugin.initialize_plugin(self, **kwargs)
-
-      self.logger.debug("Email plugin intializing started.")
-      self._plugin_details = kwargs['details']
-
-      self.prediction_date = kwargs['prediction_date']
-      self.execution_date = kwargs['execution_date']
-      self.ensemble_tests = kwargs['ensemble_tests']
-      self.password_ini_file = self._plugin_details.get('Settings', 'password_ini_file')
-
-      self.result_outfile = self._plugin_details.get("Settings", "results_outfile")
-      self.results_template = self._plugin_details.get("Settings", "results_template")
-      self.report_url = self._plugin_details.get("Settings", "report_url")
-
-      self._run_logger_conf = self._plugin_details.get('Settings', 'logging')
-
-
-      self.logger.debug("Email plugin intializing finished.")
+      details = kwargs['details']
+      self.mailhost = details.get("Settings", "mailhost")
+      self.mailport = None
+      self.fromaddr = details.get("Settings", "fromaddr")
+      self.toaddrs = details.get("Settings", "toaddrs").split(',')
+      self.subject = details.get("Settings", "subject")
+      self.user = details.get("Settings", "user")
+      self.password = details.get("Settings", "password")
+      self.result_outfile = details.get("Settings", "results_outfile")
+      self.results_template = details.get("Settings", "results_template")
+      self.report_url = details.get("Settings", "report_url")
       return True
     except Exception as e:
       self.logger.exception(e)
     return False
 
-  def run(self):
-    logger = None
-    start_time = time.time()
+  def emit(self, **kwargs):
+    if self.logger:
+      self.logger.debug("Starting emit for email output.")
     try:
-      logging.config.fileConfig(self._run_logger_conf)
-      logger = logging.getLogger()
-      logger.debug("run started.")
-    except Exception as e:
-      import traceback
-      traceback.print_exc(e)
-      if logger is not None:
-        logger.exception(e)
+      mytemplate = Template(filename=self.results_template)
+      file_ext = os.path.splitext(self.result_outfile)
+      file_parts = os.path.split(file_ext[0])
+      #Add the prediction date into the filename
+      file_name = "%s-%s%s" % (file_parts[1], kwargs['prediction_date'].replace(':', '_').replace(' ', '-'), file_ext[1])
+      out_filename = os.path.join(file_parts[0], file_name)
+      with open(out_filename, 'w') as report_out_file:
+        report_url = '%s/%s' % (self.report_url, file_name)
+        results_report = mytemplate.render(ensemble_tests=kwargs['ensemble_tests'],
+                                                prediction_date=kwargs['prediction_date'],
+                                                execution_date=kwargs['execution_date'],
+                                                report_url=report_url)
+        report_out_file.write(results_report)
+    except TypeError as e:
+      if self.logger:
+        self.logger.exception(makoExceptions.text_error_template().render())
+    except (IOError,AttributeError,Exception) as e:
+      if self.logger:
+        self.logger.exception(e)
     else:
       try:
-        #Get email server settings.
-        password_config_file = ConfigParser.RawConfigParser()
-        password_config_file.read(self.password_ini_file)
+        subject = self.subject % (kwargs['prediction_date'])
+        #Now send the email.
+        smtp = smtpClass(host=self.mailhost, user=self.user, password=self.password)
+        smtp.rcpt_to(self.toaddrs)
+        smtp.from_addr(self.fromaddr)
+        smtp.subject(subject)
+        smtp.message(results_report)
+        smtp.send(content_type="html")
+      except Exception as e:
+        if self.logger:
+          self.logger.exception(e)
+    if self.logger:
+      self.logger.debug("Finished emit for email output.")
 
-
-        self.mailhost = password_config_file.get("email_report_output_plugin", "mailhost")
-        self.mailport = None
-        self.fromaddr = password_config_file.get("email_report_output_plugin", "fromaddr")
-        self.toaddrs = password_config_file.get("email_report_output_plugin", "toaddrs").split(',')
-        self.subject = password_config_file.get("email_report_output_plugin", "subject")
-        self.user = password_config_file.get("email_report_output_plugin", "user")
-        self.password = password_config_file.get("email_report_output_plugin", "password")
-
-        mytemplate = Template(filename=self.results_template)
-        file_ext = os.path.splitext(self.result_outfile)
-        file_parts = os.path.split(file_ext[0])
-        #Add the prediction date into the filename
-        file_name = "%s-%s%s" % (file_parts[1], self.prediction_date.replace(':', '_').replace(' ', '-'), file_ext[1])
-        out_filename = os.path.join(file_parts[0], file_name)
-        logger.debug("Opening output file: %s" % (out_filename))
-        with open(out_filename, 'w') as report_out_file:
-          report_url = '%s/%s' % (self.report_url, file_name)
-          results_report = mytemplate.render(ensemble_tests=self.ensemble_tests,
-                                                  prediction_date=self.prediction_date,
-                                                  execution_date=self.execution_date,
-                                                  report_url=report_url)
-          report_out_file.write(results_report)
-      except TypeError as e:
-          logger.exception(makoExceptions.text_error_template().render())
-      except (IOError,AttributeError,Exception) as e:
-          logger.exception(e)
-      else:
-        try:
-          logger.debug("Emailing output file: %s" % (out_filename))
-          subject = self.subject % (self.prediction_date)
-          #Now send the email.
-          smtp = smtpClass(host=self.mailhost, user=self.user, password=self.password)
-          smtp.rcpt_to(self.toaddrs)
-          smtp.from_addr(self.fromaddr)
-          smtp.subject(subject)
-          smtp.message(results_report)
-          smtp.send(content_type="html")
-        except Exception as e:
-            logger.exception(e)
-    logger.debug("Finished email plugin run in %s seconds" % (time.time()-start_time))
-
-  def finalize(self):
-    return
